8639c5183fdb87c112e2ecb53a74eac152281ad0
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2025-09-03 09:45:54 +0200

    Oppdater editor markdown mal med endringer fra brevmalene ellers (tlf og lov)

--- editor/src/App.tsx
+++ editor/src/App.tsx
@@ -115,9 +115,9 @@ function App() {
     
     ## Du kan klage på vedtaket
     
-    Hvis du mener vedtaket er feil, kan du klage innen seks uker fra den datoen vedtaket har kommet fram til deg. Dette følger av [sett inn lovhenvisning]. Du finner skjema og informasjon på [nav.no/klage](https://nav.no/klage). Merk at klagen vil bli behandlet med utgangspunkt i saken slik den står nå, så hvis saken mangler opplysninger, er det bedre å søke på nytt.
+    Hvis du mener vedtaket er feil, kan du klage innen seks uker fra den datoen vedtaket har kommet fram til deg. Dette følger av folketrygdlovens § 21-12. Du finner skjema og informasjon på [nav.no/klage](https://nav.no/klage). Merk at klagen vil bli behandlet med utgangspunkt i saken slik den står nå, så hvis saken mangler opplysninger, er det bedre å søke på nytt.
     
-    Nav kan veilede deg på telefon om hvordan du sender en klage. Nav-kontoret ditt kan også hjelpe deg med å skrive en klage. Kontakt oss på telefon 55 55 33 33 \\<34\\> hvis du trenger hjelp.
+    Nav kan veilede deg på telefon om hvordan du sender en klage. Nav-kontoret ditt kan også hjelpe deg med å skrive en klage. Kontakt oss på telefon 55 55 11 11 hvis du trenger hjelp.
     
     Hvis du får medhold i klagen, kan du få dekket vesentlige utgifter som har vært nødvendige for å få endret vedtaket, for eksempel hjelp fra advokat. Du kan ha krav på fri rettshjelp etter rettshjelploven. Du kan få mer informasjon om denne ordningen hos advokater, statsforvalteren eller Nav. Du kan lese om saksomkostninger i forvaltningsloven § 36.
     
@@ -127,7 +127,7 @@ function App() {
     
     ## Du har rett til innsyn i saken din
     
-    Du har rett til å se dokumentene i saken din. Dette følger av forvaltningsloven § 18. Kontakt oss om du vil se dokumentene i saken din. Ta kontakt på [nav.no/kontakt](https://nav.no/kontakt) eller på telefon 55 55 33 33 \\<34\\>. Du kan lese mer om innsynsretten på [nav.no/personvernerklaering](https://nav.no/personvernerklaering).
+    Du har rett til å se dokumentene i saken din. Dette følger av forvaltningsloven § 18. Kontakt oss om du vil se dokumentene i saken din. Ta kontakt på [nav.no/kontakt](https://nav.no/kontakt) eller på telefon 55 55 11 11. Du kan lese mer om innsynsretten på [nav.no/personvernerklaering](https://nav.no/personvernerklaering).
     
     Vi har dessverre ikke mulighet til å vise foresatt eller verge saken og tilhørende dokumenter på [nav.no](https://nav.no). Du kan ta kontakt med oss hvis du trenger innsyn i saken din.
     
@@ -137,12 +137,11 @@ function App() {
     
     På [nav.no/kontakt](https://nav.no/kontakt) kan du chatte eller skrive til oss.
     
-    Hvis du ikke finner svar på [nav.no](https://nav.no), kan du ringe oss på telefon 55 55 33 33 \\<34\\> hverdager 09.00-15.00.
+    Hvis du ikke finner svar på [nav.no](https://nav.no), kan du ringe oss på telefon 55 55 11 11 hverdager 09.00-15.00.
     
     Med vennlig hilsen
+    Jon Åsen, Kari Hansen
     Nav hjelpemiddelsentral Agder
-    55 55 11 11
-    Skytterheia 1, 4790 LILLESAND
   `.trim();
 
   return (
