fc22c18e81b3f24f6a7cb8f78c3d146ae10b9d6e
Kristoffer A. Iversen <kristoffer.andreas.iversen@nav.no>
2025-10-30 16:35:02 +0100

    Bruk https for nav.no linker også ved uppercase bokstaver

--- editor/src/brev/breveditor/plugins/flytende-link-verktøylinje/urlTransform.ts
+++ editor/src/brev/breveditor/plugins/flytende-link-verktøylinje/urlTransform.ts
@@ -1,6 +1,6 @@
 export const urlTransform = (url: string) => {
   if (!/^[^:]+:\/\//.test(url)) {
-    if (/^nav\.no/.test(url)) return `https://${url}`;
+    if (/^nav\.no/i.test(url)) return `https://${url}`;
     return `http://${url}`;
   }
   return url;
