d3e805e20988053f405b7c5a5b2375ac66a72003
Christian Bjørnsrud <christian.kristensen.bjornsrud@nav.no>
2026-07-15 12:18:59 +0200

    Add test of eq helper in template service.

---
+++ app/src/test/kotlin/no/nav/hjelpemidler/pdfgen/template/TemplateServiceTest.kt
@@ -0,0 +1,35 @@
+package no.nav.hjelpemidler.pdfgen.template
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.io.StringWriter
+
+class TemplateServiceTest {
+    private val templateService = TemplateService()
+
+    @Test
+    fun `eq returns true when values are equal`() {
+        val writer = StringWriter()
+
+        templateService.compile(
+            "{{#if (eq first second)}}same{{else}}different{{/if}}",
+            mapOf("first" to "abc", "second" to "abc"),
+            writer,
+        )
+
+        assertEquals("same", writer.toString())
+    }
+
+    @Test
+    fun `eq returns false when values are different`() {
+        val writer = StringWriter()
+
+        templateService.compile(
+            "{{#if (eq first second)}}same{{else}}different{{/if}}",
+            mapOf("first" to "abc", "second" to "def"),
+            writer,
+        )
+
+        assertEquals("different", writer.toString())
+    }
+}
